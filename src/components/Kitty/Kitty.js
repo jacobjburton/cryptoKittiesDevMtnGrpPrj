6a485954b9d2a4cbe2cbdee76d7ca7eec14acbbe
Jacob Burton <jacobburton@gmail.com>
2018-05-29 13:58:24 -0600

    routes setup, work on header component

---
+++ src/components/Kitty/Kitty.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Kitty extends Component
+{
+    render ()
+    {
+        return (
+            <div>
+                Kitty
+            </div>
+        )
+    }
+}
+
+export default Kitty;
---
+++ src/components/Privacy-policy/Privacy-policy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PrivacyPolicy ()
+{
+    return (
+        <div>
+            Privacy Policy
+        </div>
+    )
+}
+
+export default PrivacyPolicy;
