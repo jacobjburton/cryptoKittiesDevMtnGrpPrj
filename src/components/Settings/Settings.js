12f8997878299e6901176c161908e53f90b11955
Chandler Haueter <chandlerhaueter@gmail.com>
2018-06-05 08:38:36 -0600

    Cats filter in reducer

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { getUser, getKitty, getUserActivity, getSpecialCats, getForsaleSiringOther } from '../../ducks/reducer';
+import { getUser, getKitty, getUserActivity, getSpecialCats, getForsaleSiringOther, getFilteredCats } from '../../ducks/reducer';
 import './Settings.css';
 
 class Settings extends Component {
@@ -25,22 +25,20 @@ class Settings extends Component {
         this.state.account && this.props.getUser(this.state.account)
     }
     render(){
-        let { user, cattributes, myKitties, kitty, userActivity, specialCats, forsaleSiringOther } = this.props;
+        let { user, cattributes, myKitties, kitty, userActivity, specialCats, filteredCats } = this.props;
     return (
        <div>
             <div>
                 <button onClick={(e)=>this.props.getUser(this.state.account)}>getUser</button>
                 <p>{ user.address ?  user.address : null }</p>
                 <p>{ cattributes[0] ? cattributes[0].description : null }</p>
-                <p>{ myKitties.kitties ? myKitties.kitties[0].id : null }</p>
+                <p>{ myKitties.kitties ? myKitties.kitties[0].enhanced_cattributes[0].type: null }</p>
                 <button onClick={(e)=>this.props.getKitty(myKitties.kitties[0].id)}>getKitty</button>
                 <p>{ kitty.id ? kitty.matron.color : null }</p>
                 <button onClick={(e)=>this.props.getSpecialCats()}>getSpecialCats</button>
                 <p>{ specialCats[0] ? specialCats[0].name : null }</p>
-                <button onClick={(e)=>this.props.getForsaleSiringOther()}>getForsaleSiringOther</button>
-                <p>{ forsaleSiringOther.kitties ? forsaleSiringOther.kitties[0].created_at : null }</p>
-            </div>
-            <div>
+                <button onClick={(e)=>this.props.getFilteredCats('0x4541e1ea15fab6920b6f0da0c31da0fea74e3535','normal','','','sale','sire','other','id','desc')}>getFilteredCats</button>
+                <p>{ filteredCats.kitties ? filteredCats.kitties[0].enhanced_cattributes[0].type : null }</p>
             </div>
        </div>
         )
@@ -54,9 +52,9 @@ function mapStateToProps(state) {
         kitty: state.kitty,
         userActivity: state.userActivity,
         specialCats: state.specialCats,
-        forsaleSiringOther: state.forsaleSiringOther
+        filteredCats: state.filteredCats
     }
 }
 
-export default connect(mapStateToProps, {getUser, getKitty, getUserActivity, getSpecialCats, getForsaleSiringOther})(Settings);
+export default connect(mapStateToProps, {getUser, getKitty, getUserActivity, getSpecialCats, getFilteredCats})(Settings);
 
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -7,14 +7,14 @@ const initialState = {
     myKitties: {},
     kitty: {},
     specialCats: [],
-    forsaleSiringOther: {}
+    filteredCats: {}
 }
 
 const GET_USER = 'GET_USER';
 const GET_USER_ACTIVITY = 'GET_USER_ACTIVITY';
 const GET_KITTY = 'GET_KITTY';
 const GET_SPECIAL_CATS = 'GET_SPECIAL_CATS';
-const GET_FOR_SALE_SIRING_OTHER = 'GET_FOR_SALE_SIRING_OTHER';
+const GET_FILTERED_CATS = 'GET_FILTERED_CATS';
 
 export function getUser(user_address) {
     console.log(`rdr.getUser>${user_address}`);
@@ -94,46 +94,30 @@ export function getSpecialCats() {
     }
 }
 
-export function getForsaleSiringOther() {
-    let sale = "sale";
-    let sire = "sire";
-    let other = "other";
-    let handlesData = axios.get(`https://api.cryptokitties.co/v2/kitties?offset=0&limit=12&owner_wallet_address=0x4541e1ea15fab6920b6f0da0c31da0fea74e3535&include=${sale, sire, other}&parents=false&authenticated=true&orderBy=id&orderDirection=desc`)
+export function getFilteredCats(owner_wallet_address, type, gen, cooldown, sale, sire, other, orderBy, orderDirection) {
+    //Search Marketplace: use empty string for owner_wallet_address
+    //Search MyKitties: include owner_wallet_address
+    let a = owner_wallet_address;
+    let b = type;
+    let c = gen;
+    let d = cooldown;
+    let e = sale;
+    let f = sire;
+    let g = other;
+    let h = orderBy; // ie. generation
+    let i = orderDirection;
+    let handlesData = axios.get(`https://api.cryptokitties.co/v2/kitties?offset=0&limit=12&owner_wallet_address=${a}&search=type:${b}+gen:${c}+cooldown:${d}&parents=false&authenticated=true&include=${e},${f},${g}&orderBy=${h}&orderDirection=${i}`)
     .then(res => {
-            console.log('rdr.getForsaleSiringOther>res.data', res.data);
+            console.log('rdr.getFilteredCats>res.data', res.data);
             return res.data
         })
         .catch(err => { 
-            console.log('rdr.getForsaleSiringOther>', err) });
+            console.log('rdr.getFilteredCats>', err) });
     return {
-        type: GET_FOR_SALE_SIRING_OTHER,
+        type: GET_FILTERED_CATS,
         payload: handlesData
     }
 }
-
-
-// export function getFilteredCats() {
-//     let baseUrl = `https://api.cryptokitties.co/v2/kitties`;
-//     let offset = `0`;
-//     let limit = `12`;
-//     let owner_wallet_address = `0x4541e1ea15fab6920b6f0da0c31da0fea74e3535`;
-//     let include = `sale, sire, other`;
-//     let parents = `false`;
-//     let authenticated = true;
-//     let orderBy = `id`;
-//     let orderDirection = `desc`;
-//     let handlesData = axios.get(`${baseUrl}?offset=${offset}&limit=${limit}&owner_wallet_address=${owner_wallet_address}&include=${include}&parents=${parents}&authenticated=${authenticated}&orderBy=${orderBy}&orderDirection=${orderDirection}`)
-//     .then(res => {
-//             console.log('rdr.getForsaleSiringOther>res.data', res.data);
-//             return res.data
-//         })
-//         .catch(err => { 
-//             console.log('rdr.getForsaleSiringOther>', err) });
-//     return {
-//         type: GET_FOR_SALE_SIRING_OTHER,
-//         payload: handlesData
-//     }
-// }
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
@@ -153,8 +137,8 @@ export default function reducer(state = initialState, action) {
         case GET_SPECIAL_CATS + '_FULFILLED':
             return Object.assign({}, state, {specialCats: action.payload});
 
-        case GET_FOR_SALE_SIRING_OTHER + '_FULFILLED':
-            return Object.assign({}, state, {forsaleSiringOther: action.payload});
+        case GET_FILTERED_CATS + '_FULFILLED':
+            return Object.assign({}, state, {filteredCats: action.payload});
 
         default:
             return state;
